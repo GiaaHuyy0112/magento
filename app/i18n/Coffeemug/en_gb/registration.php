cb411d22f05a599f7ff25e3e0afaf93c6c1780e5
GiaaHuyy0112 <huylag159@gmail.com>
2022-12-30 14:21:55 +0700

    Lesson 6

---
+++ app/i18n/Coffeemug/en_gb/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'coffeemug_en_gb',
+    __DIR__
+);
---
+++ app/i18n/Coffeemug/en_us/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'coffeemug_en_us',
+    __DIR__
+);
