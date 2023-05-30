5a5cb1b8779b49aa6f0a6b3421a94229cac99579
GiaaHuyy0112 <huylag159@gmail.com>
2023-05-30 17:55:41 +0700

    Unit03 fix

--- app/code/CoffeeMug/Unit03/Plugin/LayoutLoggedXml.php
+++ app/code/CoffeeMug/Unit03/Plugin/LayoutLoggedXml.php
@@ -4,6 +4,7 @@ namespace CoffeeMug\Unit03\Plugin;
 
 use Magento\Framework\View\LayoutInterface;
 use Psr\Log\LoggerInterface;
+use \Magento\Framework\App\Request\Http;
 
 class LayoutLoggedXml
 {
@@ -12,18 +13,24 @@ class LayoutLoggedXml
      */
     protected $_logger;
 
+    protected $_request;
+
     /**
      * LogPageOutput constructor.
      * @param LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, Http $request)
     {
         $this->_logger = $logger;
+        $this->_request = $request;
     }
     public function afterGenerateXml(LayoutInterface $subject, $result)
     {
-        $this->_logger->debug("Logger");
-        $this->_logger->debug(json_encode($subject->getUpdate()->asSimplexml()));
+
+
+        if ($this->_request->getFullActionName() == 'catalog_product_view') {
+            $this->_logger->debug(json_encode($subject->getUpdate()->asSimplexml()));
+        }
 
         return $result;
     }
